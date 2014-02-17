96b666e688801bcb89046ec819e71b51f7013c02
Mateusz Łapsa-Malawski <mateusz@munhitsu.com>
2014-02-17 15:48:49 +0000

    config location

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 cotton
 ======
 Project independent shared fabric extensions code
+
+
+Assumes that your config directory is next to directory containing fabfile.py:
+
+root/
+|-- application-deployment/
+|   `-- fabfile.py
+`-- config/
--- cotton/config.py
+++ cotton/config.py
@@ -1,5 +1,16 @@
 import os
 import sys
+from fabric.api import env
+"""
+takes config from "/etc/config" (jenkins server usage)
+or assumes that your config directory is next to directory containing fabfile.py:
+
+root/
+|-- application-deployment/
+|   `-- fabfile.py
+`-- config/
+
+"""
 
 
 def file_readable(filename):
@@ -15,7 +26,7 @@ CONFIG_DIR = "/etc/cotton"
 if CONFIG_DIR not in sys.path and os.path.isdir(CONFIG_DIR) and file_readable(os.path.join(CONFIG_DIR, "__init__.py")):
     sys.path.append(CONFIG_DIR)
 else:
-    CONFIG_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../config"))
+    CONFIG_DIR = os.path.abspath(os.path.join(os.path.dirname(env['real_fabfile']), "../config"))
     if CONFIG_DIR not in sys.path:
         sys.path.append(CONFIG_DIR)
 

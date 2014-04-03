7ab4e0a70fbea6756f013a180af7cc17b9a8558c
Ash Berlin <ash_github@firemirror.com>
2014-04-03 17:41:02 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cotton',
-    version='0.1.1-alpha',
+    version='0.1.2',
     url='http://github.com/ministryofjustice/cotton',
     license='TBD',
     author='',

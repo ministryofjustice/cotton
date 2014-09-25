0f6b06cb8770c01550431c0dd17cb594bb2f0426
paulrhodes <paul.rhodes@digital.justice.gov.uk>
2014-09-25 17:10:42 +0100

    Abstract AWS tagging of env/project to api.py

--- cotton/provider/aws/driver.py
+++ cotton/provider/aws/driver.py
@@ -81,10 +81,10 @@ class AWSProvider(Provider):
 
         instance.add_tag("Name", name)
         instance.add_tag("creator", getpass.getuser())
-	if 'environment' in env:
-            instance.add_tag("env", env.environment)
-	if 'project' in env:
-            instance.add_tag("project", env.project)
+
+        if 'tags' in kwargs:
+            for key in kwargs['tags']:
+                instance.add_tag(key, kwargs['tags'][key])
 
         print("Waiting for instance to run",)
         while instance.state != 'running':

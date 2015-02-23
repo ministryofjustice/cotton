dd20b0011600dddd23a85fa70319e1ee69101261
Mateusz Łapsa-Malawski <m@cr3.io>
2015-02-23 17:37:11 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cotton',
-    version='0.3.3',
+    version='0.5.0',
     url='http://github.com/ministryofjustice/cotton',
     license='TBD',
     author='',
